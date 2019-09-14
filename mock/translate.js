0970be03cf7f8c003f66eab64f917373ac22c1d4
林日三 <linrisan@meituan.com>
2019-09-14 23:01:18 +0800

    remove 金山翻译重复的配置

--- mock/translate.js
+++ mock/translate.js
@@ -6,27 +6,27 @@ import { jinshanApi } from '../src/pages/common/jinshan';
 const { ApiPrefix } = Constant
 
 module.exports = {
-  [`POST ${ApiPrefix}/translate/jinshan`](req, res) {
-    // 金山翻译
-    const { title, content } = req.body
-    const titleReq = jinshanApi(title)
-    const contentArr = content.split('\r\n');
-    const contentArrReq = contentArr.filter(item => !!item).map(item => jinshanApi(item))
-    Promise.all([titleReq, ...contentArrReq]).then(([titleRes, ...contentRes]) => {
-      res.status(200).json({
-        data: {
-          title: titleRes && titleRes.content && titleRes.content.out,
-          content: contentRes && contentRes.map(item => item.content && item.content.out)
-        }
-      })
-    }).catch((err) => {
-      res.status(400).json({
-        data: {
-          error: err
-        }
-      })
-    })
-  },
+  // [`POST ${ApiPrefix}/translate/jinshan`](req, res) {
+  //   // 金山翻译
+  //   const { title, content } = req.body
+  //   const titleReq = jinshanApi(title)
+  //   const contentArr = content.split('\r\n');
+  //   const contentArrReq = contentArr.filter(item => !!item).map(item => jinshanApi(item))
+  //   Promise.all([titleReq, ...contentArrReq]).then(([titleRes, ...contentRes]) => {
+  //     res.status(200).json({
+  //       data: {
+  //         title: titleRes && titleRes.content && titleRes.content.out,
+  //         content: contentRes && contentRes.map(item => item.content && item.content.out)
+  //       }
+  //     })
+  //   }).catch((err) => {
+  //     res.status(400).json({
+  //       data: {
+  //         error: err
+  //       }
+  //     })
+  //   })
+  // },
   [`POST ${ApiPrefix}/translate/google`](req, res) {
     // google 翻译API
     const { title, content } = req.body
